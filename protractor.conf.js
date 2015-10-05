bc52936946df0c77253f426af2f5b6d5842a11e1
Josh Burlison <jburliso@Red-Hat-jwaldman-OSX.local>
2015-10-04 22:17:16 -0500

    Point conf to jenkins selenium

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,8 +1,8 @@
 exports.config = {
   framework: 'mocha',
   // The file path to the selenium server jar ()
-  seleniumServerJar: '/usr/local/Cellar/nvm/0.26.1/versions/node/v0.12.7/lib/node_modules/protractor/selenium/selenium-server-standalone-2.47.1.jar',
-  // seleniumAddress: 'http://localhost:4444/wd/hub',
+  // seleniumServerJar: '/usr/local/Cellar/nvm/0.26.1/versions/node/v0.12.7/lib/node_modules/protractor/selenium/selenium-server-standalone-2.47.1.jar',
+  seleniumServerJar:  '/home/jenkins/.nvm/versions/node/v0.12.7/lib/node_modules/protractor/selenium/selenium-server-standalone-2.47.1.jar',
   specs: ['src/client/test/e2e/spec.js'],
   chromeOnly: true,
   capabilities: {
